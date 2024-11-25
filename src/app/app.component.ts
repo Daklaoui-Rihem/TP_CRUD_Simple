2eb43971c0b7d2eae722bb04433fbf782229e48f
Rihem Daklaoui <rihemdaklaoui@gmail.com>
2024-11-25 20:57:01 +0100

    Fisrt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'crud';
-}
+ import { EmployeeService } from '../app/employee.service';
+ import { Employee } from '../app/employee';
+ @Component({
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
+ })
+ export class AppComponent {
+  itle = 'app';
+ employees = new Array<Employee>();
+ constructor(empService:EmployeeService) {
+ empService.getEmployees().subscribe(response =>this.employees= response )
+ }
+ }
